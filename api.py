d9acb6f166d3a3a83dd25e1bdd5c328ac55fedf4
William <xinyuwei@umass.edu>
2022-04-10 16:10:08 -0700

    Tutorial1&2

---
+++ api.py
@@ -0,0 +1,54 @@
+# api.py
+from parse import parse
+from webob import Request, Response
+import inspect
+
+
+class API:
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, path):
+        if path in self.routes:
+            raise AssertionError("Such route already exists.")
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+
+        response = self.handle_request(request)
+
+        return response(environ, start_response)
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+
+        return None, None
+
+    def handle_request(self, request):
+        response = Response()
+
+        handler, kwargs = self.find_handler(request_path=request.path)
+
+        if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method now allowed", request.method)
+
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
+
+        return response
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
