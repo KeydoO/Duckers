27294939ceae60a5fa6b46749db9b6dcf49aee94
William <xinyuwei@umass.edu>
2022-04-14 14:25:24 -0700

    Tutorial4

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from api import API
+from middleware import Middleware
 
 app = API()
 
+    
+def custom_exception_handler(request, response, exception_cls):
+    response.text = "Oops! Something went wrong. Please, contact our customer support at +1-202-555-0127."
+app.add_exception_handler(custom_exception_handler)
+
+
+class SimpleCustomMiddleware(Middleware):
+    def process_request(self, req):
+        print("Processing request", req.url)
+
+    def process_response(self, req, res):
+        print("Processing response", req.url)
+
+app.add_middleware(SimpleCustomMiddleware)
+
+
 @app.route("/home")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be user")
+    
 def home(request, response):
     response.text = "Hello from the HOME page"
 
@@ -28,5 +48,5 @@ class BooksHandler:
         
 @app.route("/template")
 def template_handler(req, resp):
-    resp.body = app.template("index.html", context={"name": "Alcazar", "title": "Best Framework"}).encode()
+    resp.body = app.template("index.html", context={"name": "Duckers", "title": "Best Framework"}).encode()
 
